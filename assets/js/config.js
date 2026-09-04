7cdec30294b958a31c3b1ff05d8606e65d820a28
robbituz-debug <deploy@robbit.uz>
2026-09-04 04:58:16 +0000

    Yandex Metrika: rasmiy to'liq kod (tag.js?id, ssr, ecommerce, noscript)

--- assets/js/config.js
+++ assets/js/config.js
@@ -22,15 +22,11 @@ window.ROBBIT_CONFIG = {
   fbq('init', id, {}, { autoConfig: true }); fbq('track', 'PageView');
 })();
 
-/* Yandex Metrika — Webvisor, scroll xaritasi, forma analitikasi */
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();
-for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-ym(112271911, "init", {
-  clickmap: true,
-  trackLinks: true,
-  accurateTrackBounce: true,
-  webvisor: true
-});
+/* Yandex.Metrika counter (rasmiy kod, ID 112271911) — Webvisor, clickmap, forma analitikasi */
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=112271911', 'ym');
+ym(112271911, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
