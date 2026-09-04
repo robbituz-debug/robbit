57abacec1a888415f3cf507aa0b71868a74bc9a8
robbituz-debug <deploy@robbit.uz>
2026-09-04 04:56:25 +0000

    Yandex Metrika ulandi (ID 112271911): Webvisor, scroll xaritasi, forma analitikasi; CSP webvisor domenlari

--- assets/js/config.js
+++ assets/js/config.js
@@ -21,3 +21,16 @@ window.ROBBIT_CONFIG = {
   (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', id, {}, { autoConfig: true }); fbq('track', 'PageView');
 })();
+
+/* Yandex Metrika — Webvisor, scroll xaritasi, forma analitikasi */
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ym(112271911, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true
+});
